817d184962a0c28ae3e6b0493de0d61e5565d028
Paima <paimapurba@gmail.com>
2025-03-15 16:44:00 +0700

    add brand list

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -6,6 +6,30 @@ function Home() {
     return(<>
         <div className="home-content">
             <Banner/>
+            <div className="brand-div">
+                <p>Brand</p>
+                <div className="brand-list">
+                    <div className="first-row">
+                        <div className="brand-item">
+                            <img src="./public/the-executive-logo.png" alt="" className="the-executive-logo"/>
+                        </div>
+                        <div className="brand-item">
+                            <img src="./public/thanksinsomnia-logo.jpeg" alt="" className="thanksinsomnia-logo"/>
+                        </div>
+                        <div className="brand-item">
+                            <img src="./public/erigo-logo.jpg" alt="" className="erigo-logo"/>
+                        </div>
+                    </div>
+                    <div className="second-row">
+                        <div className="brand-item">
+                        <img src="./public/kanky-logo.jpeg" alt="" className="kanky-logo"/>
+                        </div>
+                        <div className="brand-item">
+                        <img src="./public/compass-logo.webp" alt="" className="compass-logo"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </>
     )
